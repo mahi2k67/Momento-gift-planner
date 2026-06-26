1bc46c71b14691728af51474bc00467b4f56cbd7
MAHALAKSHMI <mahalakshmidhanushkodii@gmail.com>
2026-06-26 17:47:02 +0530

    Add Category and Gift tables with foreign key relationship

--- database/monento.sql
+++ database/monento.sql
@@ -21,3 +21,44 @@ VALUES(
 );
 
 SELECT * FROM User;
+
+CREATE TABLE Category (
+    category_id INT AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(100) NOT NULL
+);
+
+INSERT INTO Category(category_name)
+VALUES
+('Flowers'),
+('Chocolates'),
+('Soft Toys'),
+('Personalized Gifts'),
+('Books');
+
+SELECT * FROM Category;
+
+CREATE TABLE Gift (
+    gift_id INT AUTO_INCREMENT PRIMARY KEY,
+    gift_name VARCHAR(100) NOT NULL,
+    description TEXT,
+    price DECIMAL(10,2),
+    stock INT,
+    category_id INT,
+    FOREIGN KEY (category_id)
+        REFERENCES Category(category_id)
+);
+INSERT INTO Gift(gift_name,description,price,stock,category_id)
+VALUES
+('Rose Bouquet','Fresh red roses',499.00,20,1),
+('Chocolate Box','Premium chocolates',799.00,15,2),
+('Teddy Bear','Soft toy',999.00,10,3);
+
+SELECT * FROM Gift;
+
+SELECT
+Gift.gift_name,
+Category.category_name,
+Gift.price
+FROM Gift
+JOIN Category
+ON Gift.category_id = Category.category_id;
